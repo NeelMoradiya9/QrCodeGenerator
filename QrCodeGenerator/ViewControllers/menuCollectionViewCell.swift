48f5a732661114e2ff8e176e07110bae7f71f879
NeelMoradiya9 <moradiyaneel4303@gmail.com>
2024-03-14 15:45:13 +0530

    Generator Added

---
+++ QrCodeGenerator/ViewControllers/menuCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  menuCollectionViewCell.swift
+//  QrCodeGenerator
+//
+//  Created by Neel  on 09/03/24.
+//
+
+import UIKit
+
+class menuCollectionViewCell: UICollectionViewCell {
+    
+}
